de23d485b90aa09c36d99ee428729c4183cbb920
alexandre <alexandredeucher@gmail.com>
2023-07-26 14:47:18 -0300

    refatoração da criação de comentario

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,14 @@ class CommentsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-      @comments = Comment.all
+      debugger
+      @comments = @commentable.comments
   
       render json: @comments
     end
 
     def create
-      debugger
-      @comment = Comment.new(comment_params.merge({user: current_user, commentable: @pub || @com}))
+      @comment = Comment.new(comment_params.merge({user: current_user, commentable: @commentable}))
         if @comment.save
           render json: @comment
         else
@@ -19,25 +19,32 @@ class CommentsController < ApplicationController
       end    
     end
 
-    def set_commentable
-      if params["comment"]["publication_id"]
-        @pub = Publication.find(params["comment"]["publication_id"])
-      elsif params["comment"]["comment_id"]
-        @com = Comment.find(params["comment"]["comment_id"])
+    def update
+      if @comment.update(comment_params.merge({commentable: @commentable}))
+        render json: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
       end
     end
-  #   def set_comment
-  #       @comment = Comment.find(params[:id])
-  #   end
 
-  #   def set_publication
-  #     debugger
-  #     @publication = Publication.find(params[:publication_id])
-  # end
-  
+    def set_commentable
 
+      @commentable = if params["comment"]["publication_id"]
+                        Publication.find(params["comment"]["publication_id"])
+                      elsif params["comment"]["comment_id"]
+                        Comment.find(params["comment"]["comment_id"])
+                      end
+    end
 
     def comment_params
         params.require(:comment).permit(:comment)
     end
 end
+    #   def set_comment
+    #       @comment = Comment.find(params[:id])
+    #   end
+  
+    #   def set_publication
+    #     debugger
+    #     @publication = Publication.find(params[:publication_id])
+    # end
